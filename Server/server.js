59a4f46b7e4b303aa285230a21c1f9febe0706bc
ashksin121 <ashksin121@gmail.com>
2020-12-05 20:46:29 +0530

    course add, accept, reject endpoints added

--- Server/server.js
+++ Server/server.js
@@ -30,12 +30,98 @@ app.post('/', (req, res)=> {
 app.get('/getAllCourses', (req, res) => {
     db.collection('prepup').doc('courses').get()
     .then(doc => {
-        console.log(doc)
-        res.json(doc)
+        console.log(doc.data())
+        res.json(doc.data())
     })
     .catch(err => {
         console.log("Error: ", err)
         res.json(err)
+    })
+})
+
+/*
+    CREATE COURSE
+    req.body = {
+        "uid": "aaa1a",
+        "data": {
+            "links": [],
+            "quizzes": [],
+            "summary": "Demo Course",
+            "tags": "demo course",
+            "title": "Demo"
+        }
+    }
+*/
+
+app.post('/createCourse', (req, res) => {
+    db.collection('prepup').doc('courses').get()
+    .then(doc => {
+        let len = Object.keys(doc.data()).length + 1
+        let course = {}
+        let courseId = req.body.uid + "-" + len.toString()
+        let courseBody = req.body.data
+        courseBody['status'] = "REVIEW"
+        courseBody['instructorId'] = req.body.uid
+        courseBody['numberOfStudents'] = 0
+        course[courseId] = courseBody
+        db.collection('prepup').doc('courses').set(
+            course, { merge: true }
+        ).then(doc => {
+            console.log("Course added successfully")
+            res.status(200).send("Course added successfully")
+        })
+        .catch(err => {
+            console.log("Error: ", err)
+            res.status(400).json(err)
+        })
+    })
+    .catch(err => {
+        console.log("Error: ", err);
+        res.status(400).json(err)
+    })
+})
+
+app.patch('/acceptCourse/:id', (req, res) => {
+    const courseId = req.params.id
+    db.collection('prepup').doc('courses').get()
+    .then(doc => {
+        let course = doc.data()[courseId]
+        course['status'] = "ACCEPTED"
+        db.collection('prepup').doc('courses').update({[courseId]: course})
+        .then(doc=> {
+            console.log(doc)
+            res.status(200).json(doc)
+        })
+        .catch(err => {
+            console.log("Error: ", err)
+            res.status(400).json(err)
+        })
+    })
+    .catch(err => {
+        console.log("Error: ", err)
+        res.status(400).json(err)
+    })
+})
+
+app.patch('/rejectCourse/:id', (req, res) => {
+    const courseId = req.params.id
+    db.collection('prepup').doc('courses').get()
+    .then(doc => {
+        let course = doc.data()[courseId]
+        course['status'] = "REJECTED"
+        db.collection('prepup').doc('courses').update({[courseId]: course})
+        .then(doc=> {
+            console.log(doc)
+            res.status(200).json(doc)
+        })
+        .catch(err => {
+            console.log("Error: ", err)
+            res.status(400).json(err)
+        })
+    })
+    .catch(err => {
+        console.log("Error: ", err)
+        res.status(400).json(err)
     })
 })
 
