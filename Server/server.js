65d48b59bfc5ce2474eb5d16b5441efdee65d152
Abhishek Garain <abhi211199@gmail.com>
2020-12-06 03:22:00 +0530

    QUIZ endpoints added

--- Server/server.js
+++ Server/server.js
@@ -68,7 +68,8 @@ app.post('/createCourse', (req, res) => {
             course, { merge: true }
         ).then(doc => {
             console.log("Course added successfully")
-            res.status(200).send("Course added successfully")
+            // res.status(200).send("Course added successfully")
+            res.status(200).json(courseId);
         })
         .catch(err => {
             console.log("Error: ", err)
@@ -146,7 +147,7 @@ app.post('/createProfile', (req,res) => {
         content, { merge: true }
     );
     // console.log(data)
-    res.json("Profile created successfully!!")
+    res.status(200).json("Profile created successfully!!")
 })
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -162,15 +163,116 @@ app.get('/fetchProfile', (req,res) => {
     db.collection('prepup').doc('profiles').get()
     .then(doc => {
         console.log(doc.data()[uid])
-        res.json(doc.data()[uid])
+        res.status(200).json(doc.data()[uid])
     })
     .catch(err => {
         console.log("Error: ", err)
-        res.json(err)
+        res.status(400).json(err)
     })
 })
 //////////////////////////////////////////////////////////////////////////////////////////
 
+
+///////////////////////                     CREATE QUIZ    /////////////////////////
+// SAMPLE
+// {
+//     "uid": "aaa1a",
+//     "data": [
+//         {
+//             "answer": 1,
+//             "options": ["Rahul Gandhi", "Narendra Modi", "Sonia Gandhi", "Amit Shah"],
+//             "question": "PM of India"
+//         },
+//         {
+//             "answer": 2,
+//             "options": ["Rahul Gandhi", "Narendra Modi", "Mamata Banerjee", "Amit Shah"],
+//             "question": "CM of West Bengal"
+//         },
+//         {
+//             "answer": 0,
+//             "options": ["1957", "1959", "1948", "1953"],
+//             "question": "In which year did Ghana gain independence and ended colonialism?"
+//         }
+//     ]
+// }
+app.post('/createQuiz', (req,res) => {
+    var prepRef = db.collection('prepup').doc('quiz');
+    var uid = req.body.uid;
+    var content={};
+    content[uid]=req.body.data;
+    var setWithMerge = prepRef.set(
+        content, { merge: true }
+    );
+    // console.log(data)
+    res.status(200).json("Quiz created successfully!!")
+})
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////                     FETCH QUIZ    /////////////////////////
+// SAMPLE
+// {
+//     "uid": "aaa1a",
+// }
+app.get('/fetchQuiz', (req,res) => {
+    var uid = req.body.uid;
+    db.collection('prepup').doc('quiz').get()
+    .then(doc => {
+        console.log(doc.data()[uid])
+        res.status(200).json(doc.data()[uid])
+    })
+    .catch(err => {
+        console.log("Error: ", err)
+        res.status(400).json(err)
+    })
+})
+//////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////                     FETCH RANDOM QUIZ    /////////////////////////
+// SAMPLE
+// {
+//     "uid": "aaa1a",
+// }
+app.get('/fetchRandomQuiz', (req,res) => {
+    var uid = req.body.uid;
+    var docRef = db.collection("prepup").doc("profiles");
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            var coursesCompleted = doc.data()[uid].coursesCompleted;
+            var resp=[];
+            db.collection("prepup").doc("quiz").get().then(function(doc1) {
+                if (doc1.exists) {
+                    var quizzes=doc1.data();
+                    var i=10;
+                    
+                    coursesCompleted.map(idx=>{
+                        // console.log(quizzes[idx]);
+                        // var quiz=quizzes[idx];
+                        resp.push(quizzes[idx][Math.floor(Math.random() * quizzes[idx].length)]);
+                        i--;
+                    })
+                    var GK = quizzes["general_knowledge"];
+                    resp=resp.concat(GK.sort(() => Math.random() - Math.random()).slice(0, i));
+                    res.status(200).json(resp);
+                } else {
+                    // doc.data() will be undefined in this case
+                    res.status(400).json("Error");
+                    console.log("No such document!");
+                }
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
+                res.status(400).json("Error");
+            });
+            // res.status(200).json(resp);
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+})
+//////////////////////////////////////////////////////////////////////////////////////////
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
