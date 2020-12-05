43b7a4191d0fa35ec8d11894e674dd916cb73098
Abhishek Garain <abhi211199@gmail.com>
2020-12-05 17:43:13 +0530

    created PROFILE endpoints

--- Server/server.js
+++ Server/server.js
@@ -39,6 +39,54 @@ app.get('/getAllCourses', (req, res) => {
     })
 })
 
+///////////////////////                     CREATE PROFILE    /////////////////////////
+// SAMPLE
+// {
+//     "uid": "aaa1a",
+//     "data": {
+//         "avatar": "",
+//         "coursesCompleted": ["c201"],
+//         "coursesIncomplete": ["c203","c204"],
+//         "coursesUploaded": [],
+//         "name": "Abhi",
+//         "pepCoins": 1100
+//     }
+// }
+app.post('/createProfile', (req,res) => {
+    var prepRef = db.collection('prepup').doc('profiles');
+    var uid = req.body.uid;
+    var content={};
+    content[uid]=req.body.data;
+    var setWithMerge = prepRef.set(
+        content, { merge: true }
+    );
+    // console.log(data)
+    res.json("Profile created successfully!!")
+})
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+///////////////////////                     FETCH PROFILE    /////////////////////////
+// SAMPLE
+// {
+//     "uid": "aaa1a",
+// }
+app.get('/fetchProfile', (req,res) => {
+    var uid = req.body.uid;
+    db.collection('prepup').doc('profiles').get()
+    .then(doc => {
+        console.log(doc.data()[uid])
+        res.json(doc.data()[uid])
+    })
+    .catch(err => {
+        console.log("Error: ", err)
+        res.json(err)
+    })
+})
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
