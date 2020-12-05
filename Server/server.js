542ef315756d67bb21aa31d9fae7f1e2de317bc3
ashksin121 <ashksin121@gmail.com>
2020-12-05 14:11:48 +0530

    Firestore linked

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,16 @@ const cors = require('cors')
 
 const app = express()
 
+const admin = require('firebase-admin')
+
+const serviceAccount = require('./serviceAccountCredentials.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore()
+
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -15,6 +25,18 @@ app.get('/', (req, res)=> {
 
 app.post('/', (req, res)=> {
     res.json("POST working!!")
+})
+
+app.get('/getAllCourses', (req, res) => {
+    db.collection('prepup').doc('courses').get()
+    .then(doc => {
+        console.log(doc)
+        res.json(doc)
+    })
+    .catch(err => {
+        console.log("Error: ", err)
+        res.json(err)
+    })
 })
 
 app.listen(port, () => {
