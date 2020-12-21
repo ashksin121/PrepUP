162935bff7b26436de2f4a402e918be7b11085a8
Vaibhav <vaibhavjain30699@gmail.com>
2020-12-21 12:54:21 +0530

    Added Quiz UI

--- prep_up/lib/main.dart
+++ prep_up/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:prep_up/addQuiz.dart';
 import 'package:prep_up/addcourse.dart';
 import 'package:prep_up/courseDetails.dart';
+import 'package:prep_up/custom/quiz.dart';
 // import 'package:prep_up/home.dart';
 // import 'package:prep_up/signin.dart';
 // import 'package:prep_up/teacher.dart';
@@ -21,18 +22,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: CourseDetails(),
+      home: Quiz(),
     );
   }
 }
